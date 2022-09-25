369bd5def6ae4ae8525e4e55d688bb2b713457d7
alexmint <keklolpuck@gmail.com>
2022-09-25 20:35:13 +0300

    ДЗ2 решена задача 1

---
+++ src/HW2/Task1.java
@@ -0,0 +1,19 @@
+package HW2;
+
+public class Task1 {
+    public static void main(String[] args) {
+        byte mounth = 4;
+        if (mounth == 1) System.out.println("Январь");
+        else if (mounth == 2) System.out.println("Февраль");
+        else if (mounth == 3) System.out.println("Март");
+        else if (mounth == 4) System.out.println("Апрель");
+        else if (mounth == 5) System.out.println("Май");
+        else if (mounth == 6) System.out.println("Июнь");
+        else if (mounth == 7) System.out.println("Июль");
+        else if (mounth == 8) System.out.println("Август");
+        else if (mounth == 9) System.out.println("Сентябрь");
+        else if (mounth == 10) System.out.println("Октябрь");
+        else if (mounth == 11) System.out.println("Ноябрь");
+        else System.out.println("Декабрь");
+    }
+}
