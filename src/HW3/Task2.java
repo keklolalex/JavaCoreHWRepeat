28d6e5ac814a2bdfb9b0f96a538b09b4165b7268
alexmint <keklolpuck@gmail.com>
2022-09-27 23:10:10 +0300

    ДЗ_3 решена задача 2

--- src/HW3/Task2.java
+++ src/HW3/Task2.java
@@ -1,2 +1,12 @@
-package HW3;public class Task2 {
+package HW3;
+/*
+Вывести на экран первые 10 элементов последовательности 2 4 6 8 10 и тд
+ */
+public class Task2 {
+    public static void main(String[] args) {
+        for (int i = 0, start = 2; i < 10; i++, start += 2){ //наверное можно start перенести внутрь цикла
+            // i - это счетчик
+            System.out.println(start);
+        }
+    }
 }
