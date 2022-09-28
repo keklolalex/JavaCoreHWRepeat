301e02d93974ebb4f7246833716e9495bfcf3d67
alexmint <keklolpuck@gmail.com>
2022-09-28 16:22:50 +0300

    ДЗ_3 решена задача 5 по новой

--- src/HW3/Task5.java
+++ src/HW3/Task5.java
@@ -17,13 +17,23 @@ public class Task5 {
     public static void main(String[] args) {
         int random = (int) (1 + Math.random() * 9);
         Scanner in = new Scanner(System.in);
-        int game = in.nextInt();
-        if (game == 0) System.out.println("выход из программы " + random);
-        while (game != 0){
-            if (random > game) System.out.println("загаданное число больше " + random);
-            if (random < game) System.out.println("загаданное число меньше " + random);
-            if (random == game) System.out.println("Вы угадали " + random);
-            break;
+        while (true){
+            int game = in.nextInt();
+            if (game == 0) {
+                System.out.println("выход из программы ");
+                break;
+            }
+            else if (random > game) {
+                System.out.println("загаданное число больше");
+            }
+            else if (random < game) {
+                System.out.println("загаданное число меньше");
+            }
+            else {
+                System.out.println("Вы угадали " + random);
+                break;
+            }
+            continue;
             /* не понятен 1 момент почему не проходит такая запись break на каждое условие, можно ли break записывать с командой в одной строке
             if (random > game) System.out.println("загаданное число больше");
             else if (random < game) System.out.println("загаданное число меньше"); break;
