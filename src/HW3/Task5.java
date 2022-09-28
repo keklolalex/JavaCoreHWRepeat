9994d27d9a5f279749420086185f9b054219f073
alexmint <keklolpuck@gmail.com>
2022-09-28 12:26:20 +0300

    ДЗ_3 решена задача 5

--- src/HW3/Task5.java
+++ src/HW3/Task5.java
@@ -24,11 +24,12 @@ public class Task5 {
             if (random < game) System.out.println("загаданное число меньше " + random);
             if (random == game) System.out.println("Вы угадали " + random);
             break;
+            /* не понятен 1 момент почему не проходит такая запись break на каждое условие, можно ли break записывать с командой в одной строке
+            if (random > game) System.out.println("загаданное число больше");
+            else if (random < game) System.out.println("загаданное число меньше"); break;
+            ...
+            */
         }
     }
 }
-/* не понятен 1 момент почему не проходит такая запись break на каждое условие, можно ли break записывать с командой в одной строке
- if (random > game) System.out.println("загаданное число больше");
-        else if (random < game) System.out.println("загаданное число меньше"); break;
-        ...
- */
+
