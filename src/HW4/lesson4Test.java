bb242d60da75dc16aed9178f68ba48cd9ffc2831
alexmint <keklolpuck@gmail.com>
2022-10-02 18:21:19 +0300

    ДЗ_5 созда и 4 ДЗ протестировал все

--- src/HW4/lesson4Test.java
+++ src/HW4/lesson4Test.java
@@ -94,13 +94,90 @@ public class lesson4Test {
 
         System.out.println("#" + tip.stripTrailing() + "#");
 
+        //compareTo
+        //для Int
+        //сравнивает числовой объект, который вызывает метод, с аргументом.
+        //должны быть одного типа
+        //Если Integer равно аргументу, то возвращается 0.
+        //Если Integer меньше, чем аргумент, то возвращается -1.
+        //Если Integer больше, чем аргумент, то возвращается 1.
+        //для String
+        //Для упорядочивания объектов одного типа, хранящихся в массиве или коллекции
+        //Первый: метод сравнивает строку с другим объектом
+        //второй: метод лексически сравнивает две строки.
+
+        Integer x = 10;
+        System.out.println(x.compareTo(3));
+        System.out.println(x.compareTo(10));
+        System.out.println(x.compareTo(8));
+
+        String str1 = "задор";
+        String str2 = "зодир";
+        String str3 = "задор";
+
+        int resul = str1.compareTo(str2);
+        System.out.println(resul);
+
+        resul = str2.compareTo(str3);
+        System.out.println(resul);
+
+        resul = str3.compareTo(str1);
+        System.out.println(resul);
 
 
+        //charAt
+        //возвращает символ из массива строки по указанному индексу
+        String s = "Снова в путь";
+        char result1 = s.charAt(3);
+        char result2 = s.charAt(11);
+        System.out.println("Третий символ строки - " + result1);
+        System.out.println("Одиннадцатый символ строки - " + result2);
+
+        //compareToIgnoreCase
+        //сравнивает лексически две строки, игнорируя регистр букв
+        //возвращает отрицательное целое число, ноль или положительное целое число,
+        // если заданная строка меньше, равна или больше сравниваемой строки, игнорируя регистр
+
+        System.out.println(str1.compareToIgnoreCase(str2));
+        System.out.println(str3.compareToIgnoreCase(str1));
 
 
+        //isBlank
+        //проверяет, есть-ли в строке пробел, пустая строка ("") или значение null
+        //возвращает true только если в строке есть только пробел(-ы)!!! (и нет других символов) или строка пустая ("") или имеет значение null
+        //удаляет белые пробелы
+        String string = "     Привет";
+        String string1 = "";
+        String string2 = "Привет     ";
+        System.out.println(string.isBlank()); //false
+        System.out.println(string1.isBlank());//true
+        System.out.println(string2.isBlank()); //false
+
+        //isEmpty
+        //проверяет, является ли строка пустой ("") или значение null
+        //возвращает true только если строка абсолютно!!! пустая ("") или имеет значение null
+        //только пустые строки
+        System.out.println(string.isEmpty()); //false
+        System.out.println(string1.isEmpty()); //false
+        System.out.println(string2.isEmpty()); //false
 
 
+        //repeat
+        //string.repeat(count);
+        //повтор строк без использования цикла
+        String string4 = "Привет! ";
+        int count=5;
+        System.out.println(string4.repeat(count));
 
+        //toLowerCase
+        //делает все буквы в строке строчными
+        String stre = "DSDAD";
+        System.out.println(stre.toLowerCase());
+
+        //toUpperCase
+        //делает все буквы в строке заглавными
+        stre = "njsdf";
+        System.out.println(stre.toUpperCase());
 
     }
 }
