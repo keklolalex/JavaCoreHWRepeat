40a88b6eed7cd1d7cee65aa2d8df401a10c1b689
alexmint <keklolpuck@gmail.com>
2022-10-03 18:25:29 +0300

    ДЗ_5 задание 5 переделал

--- src/HW5/Task5.java
+++ src/HW5/Task5.java
@@ -9,32 +9,21 @@ import java.util.Arrays;
 public class Task5 {
     public static void main(String[] args) {
         int[] otr = {-1, 32, 12, 93, -5, 0, 43, -54, -654, 23, -8}; //нули не считает только
-        int plusCount = 0;
         int minusCount = 0;
         for (int i : otr) {
-            if (i > 0) plusCount++;
-            else if (i < 0) minusCount++;
+            if (i < 0) minusCount++;
         }
 
-        int[] plus = new int[plusCount];
-        System.out.println(Arrays.toString(plus));
         int[] minus = new int[minusCount];
         System.out.println(Arrays.toString(minus));
-        int plusCount1 = 0;
         int minusCount1 = 0;
 
         for (int element : otr) {
-            if (element > 0) {
-                plus[plusCount1] = element;
-                plusCount1++;
-            }
-            else if (element < 0) {
+           if (element < 0) {
                 minus[minusCount1] = element;
                 minusCount1++;
             }
         }
-        System.out.println(Arrays.toString(plus));
         System.out.println(Arrays.toString(minus));
-
         }
     }
